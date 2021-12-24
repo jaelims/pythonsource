9413bbfa48fc03fc0f2ff270db642341294bc957
jaelim <jaelimly@gmail.com>
2021-12-24 13:18:34 +0900

    ~ 12.24 python

--- type/list2.py
+++ type/list2.py
@@ -207,17 +207,117 @@ a = [1, 2, 3]
 
 
 # enumerate() : 인덱스 값 사용
-list1 = [23, 12, 36, 53, 19]
+# list1 = [23, 12, 36, 53, 19]
 
-for item in list1:
-    print(item)
+# for item in list1:
+#     print(item)
 
-print()
+# print()
 
-for item in enumerate(list1):
-    print(item)
+# for item in enumerate(list1):
+#     print(item)
 
-print()
+# print()
 
-for idx, item in enumerate(list1, start=1):
-    print("%d : %d" % (idx, item))
+# for idx, item in enumerate(list1, start=1):
+#     print("%d : %d" % (idx, item))
+
+# A 학급에 총 10명의 학생이 있다. 이 학생들의 중간고사 점수는 다음과 같다.
+# [70,60,55,75,95,90,80,85,100]
+# A 학급의 평균을 구하시오
+# list1 = [70, 60, 55, 75, 95, 90, 80, 85, 100]
+# total = 0
+# for jumsu in list1:
+#     total += jumsu
+# avg = total / len(list1)
+# print("A 학급의 평균 %.2f" % avg)
+# print("A 학급의 평균 %.2f" % (sum(list1) / len(list1)))
+
+
+# ord() : 코드 값 반환
+# print(ord("A"))  # 65
+
+# chr() : 코드값을 문자로 반환
+# print(chr(65))  # A
+
+
+# 주차장 프로그램
+# 총 5대의 차가 주차 가능 / 맨 마지막 차랑부터 빠져나갈 수 있음
+
+# parking_lot = []
+# top, car_name = 0, "A"
+
+# while True:
+#     no = int(input("[1] 자동차 넣기 | [2] 자동차 빼기 | [3] 종료 : "))
+
+#     if no <= 3:
+#         if no == 1:
+#             if top >= 5:
+#                 print("주차장 꽉 찼음")
+#             else:
+#                 parking_lot.append(car_name)
+#                 print("{} 자동차 들어감. 주차장 상태 ==> {}".format(car_name, parking_lot))
+
+#                 top += 1
+#                 car_name = chr(ord(car_name) + 1)
+#         elif no == 2:
+#             if top == 0:
+#                 print("빠져나갈 자동차가 없음")
+#             else:
+#                 outCar = parking_lot.pop()
+#                 print("{} 자동차 나감. 주차장 상태 ==> {}".format(outCar, parking_lot))
+
+#                 top -= 1
+#                 car_name = chr(ord(car_name) - 1)
+#         else:
+#             print("프로그램 종료")
+#             break
+#     else:
+#         print("번호를 확인해 주세요")
+
+
+# List Comprehension
+
+# 1 ~ 100
+
+# numbers = []
+# for n in range(1, 101):
+#     numbers.append(n)
+
+# numbers = list(range(1, 101))
+# print(numbers)
+
+# numbers = [n for n in range(1, 101)]
+# print(numbers)
+
+# list1 = ["갑", "을", "병", "정", "경"]
+# 정을 제외하고 출력
+# for s in list1:
+#     if s != "정":
+#         print(s)
+
+# list2 = [s for s in list1 if s != "정"]
+# print(list2)
+
+# 1~100까지 홀수를 리스트로 생성하고 출력
+# list1 = [x for x in range(1, 101) if x % 2 == 1]
+# print(list1)
+
+# 아래 리스트 항목 중에서 5글자 이상의 단어만 출력
+# list1 = ["nice", "study", "python", "anaconda", "java"]
+# list2 = [x for x in list1 if len(x) >= 5]
+# print(list2)
+
+# 소문자만 모아서 새로운 리스트로 생성하고 출력
+# list1 = ["A", "b", "c", "D", "E", "f", "G", "h"]
+# list2 = [a for a in list1 if a.islower()]
+# print(list2)
+
+# 각 요소의 2배를 한 리스트를 생성하고 출력
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [x * 2 for x in list1]
+# print(list2)
+
+# print([x * x for x in range(5)])
+
+print([[x, x + 1, x + 2] for x in [1, 2, 3]])
